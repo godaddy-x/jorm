18b38bc60bb7d87fc28468cfeeb492630db3d839
godaddy-x <shadowsick@sina.com>
2018-12-29 16:41:56 +0800

    1.优化异常状态定义

--- exception/exception.go
+++ exception/exception.go
@@ -13,13 +13,27 @@ import (
  */
 
 const (
-	BIZ      = 100000 // 普通业务异常
-	JSON     = 999994 // JSON转换异常
-	NUMBER   = 999995 // 数值转换异常
-	DATABASE = 999996 // 数据库服务异常
-	CACHE    = 999997 // 缓存服务异常
-	SYSTEM   = 999998 // 系统级异常
-	UNKNOWN  = 999999 // 未知异常
+	BIZ     = 100000 // 普通业务异常
+	JSON    = 999994 // JSON转换异常
+	NUMBER  = 999995 // 数值转换异常
+	DATA    = 999996 // 数据服务异常
+	CACHE   = 999997 // 缓存服务异常
+	SYSTEM  = 999998 // 系统级异常
+	UNKNOWN = 999999 // 未知异常
+)
+
+const (
+	JSON_ERR    = "响应数据构建失败"
+	DATA_ERR    = "数据服务加载失败"
+	DATA_C_ERR  = "数据保存失败"
+	DATA_R_ERR  = "数据查询失败"
+	DATA_U_ERR  = "数据更新失败"
+	DATA_D_ERR  = "数据删除失败"
+	CACHE_ERR   = "缓存服务加载失败"
+	CACHE_C_ERR = "缓存数据保存失败"
+	CACHE_R_ERR = "缓存数据读取失败"
+	CACHE_U_ERR = "缓存数据更新失败"
+	CACHE_D_ERR = "缓存数据删除失败"
 )
 
 type exception struct {
@@ -27,7 +41,10 @@ type exception struct {
 	Msg  string
 }
 
-func Throw(code int, msg ...interface{}) error {
+func Throw(err interface{}, code int, msg ...interface{}) error {
+	if code > BIZ {
+		log.Println(err)
+	}
 	if s, err := util.ObjectToJson(exception{code, util.AddStr(msg...)}); err != nil {
 		log.Println(err)
 		return errors.New(util.AddStr("异常转换失败: ", err.Error()))
