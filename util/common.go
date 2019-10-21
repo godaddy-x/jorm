58902e07adb89d6cd35721b65afc8ebff6f05608
godaddy-x <shadowsick@sina.com>
2019-10-21 18:16:10 +0800

    修正版本

--- util/common.go
+++ util/common.go
@@ -54,9 +54,7 @@ func ObjectToJson(src interface{}) (string, error) {
 // JSON字符串转对象
 func JsonToObject(src string, target interface{}) error {
 	var json = jsoniter.ConfigCompatibleWithStandardLibrary
-	d := json.NewDecoder(bytes.NewBuffer([]byte(src)))
-	d.UseNumber()
-	if err := d.Decode(target); err != nil {
+	if err := json.Unmarshal([]byte(src), target); err != nil {
 		return errors.New("Json字符串转换对象出现异常: " + err.Error())
 	}
 	return nil
